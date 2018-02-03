ab8df3fda74bc2f9c5f7883d79382584b4a0e507
papyrussolution <papyrussolution@gmail.com>
2018-02-03 15:41:09 +0300

    Version 9.9.3-pre

---
+++ Src/SLib/test-cairo.cpp
@@ -0,0 +1,9 @@
+// TEST-CAIRO.CPP
+//
+#include <slib.h>
+#include <tv.h>
+#pragma hdrstop
+//
+// @construction 
+// Test of cairo module
+//
---
+++ Src/SLib/test-openssl.cpp
@@ -0,0 +1,9 @@
+// TEST-OPENSSL.CPP
+//
+#include <slib.h>
+#include <tv.h>
+#pragma hdrstop
+//
+// @construction 
+// Test of openssl module
+//
