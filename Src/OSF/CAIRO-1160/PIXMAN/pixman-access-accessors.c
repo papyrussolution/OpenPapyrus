46acb744e3b6ba10b7ee3adae6c1dbda99e5095b
papyrussolution <papyrussolution@gmail.com>
2019-01-31 11:48:18 +0300

    Version 10.3.2-pre

---
+++ Src/OSF/CAIRO-1160/PIXMAN/pixman-access-accessors.c
@@ -0,0 +1,5 @@
+#include "cairoint.h"
+#pragma hdrstop
+
+#define PIXMAN_FB_ACCESSORS
+#include "pixman-access.c"
---
+++ Src/OSF/CAIRO-1160/PIXMAN/pixman-edge-accessors.c
@@ -0,0 +1,4 @@
+#include "cairoint.h"
+#pragma hdrstop
+#define PIXMAN_FB_ACCESSORS
+#include "pixman-edge.c"
