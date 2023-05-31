9034c0c75ef4f1bd929f86dcaf402367677dffe3
papyrussolution <papyrussolution@gmail.com>
2023-05-31 18:57:08 +0300

    Version 11.7.4

---
+++ Src/PPMain/crr32_support.cpp
@@ -0,0 +1,13 @@
+// crr32_support.cpp
+// Copyright (c) A.Sobolev 2023
+// Модуль для обслуживания вызовов к 32-разрядной библиотеке Crystal Reports
+// @codepage UTF-8
+//
+#include <pp.h>
+#pragma hdrstop
+#include <crpe.h>
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
---
+++ Src/PPTEST/pptest-dedicated.cpp
@@ -0,0 +1,12 @@
+// PPTEST-DEDICATED.CPP
+// Copyright (c) A.Sobolev 2023
+// @codepage UTF-8
+// Отдельный модуль тестирования.
+//
+#include <pp.h>
+#pragma hdrstop
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
