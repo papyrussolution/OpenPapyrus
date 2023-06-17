1b632892e44876deb62c2c451164530d11b485db
papyrussolution <papyrussolution@gmail.com>
2023-06-17 11:58:58 +0300

    Version 11.7.6-pre

---
+++ Src/OSF/ASMLIB/fixedloop.asm
@@ -0,0 +1,44 @@
+COMMENT @
+Copyright (c) 2022, Intel Corporation All rights reserved. 
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+  disclaimer. 
+* Neither the name of Intel Corporation nor the names of its contributors may be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+@
+
+ifndef X64
+.686p
+.model flat
+endif
+
+_text SEGMENT
+
+align 16
+
+fixed_loop PROC
+	mov edx, -1
+	mov eax, 2
+x:
+	and ecx, edx
+	sub ecx, eax
+	jg x
+	ret
+fixed_loop ENDP
+
+; alias for 32-bit fastcall function. We can reuse the same code since both 32-bit fastcall and 64-bit ABI use RCX/ECX to pass the first parameter
+alias <@fixed_loop@4> = <fixed_loop>
+
+_text ENDS
+END
